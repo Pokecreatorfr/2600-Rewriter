c4a0927e3570a443712c4cdee59873b9a1e58d51
Mathéo Vignaud <pokecreatorfr@gmail.com>
2024-08-21 21:47:56 +0200

    Implémentation de toutes les instructions
    à l'aide de http://6502.org/tutorials/6502opcodes.html

--- include/instruction_set.hpp
+++ include/instruction_set.hpp
@@ -10,10 +10,14 @@ struct Mem2600
     uint8_t A;
     uint8_t X;
     uint8_t Y;
-    bool N;
-    bool V;
-    bool Z;
     bool C;
+    bool Z;
+    bool I;
+    bool D;
+    bool B;
+    // inused bit
+    bool V;
+    bool N;
     uint16_t PC;
     uint8_t SP;
     uint8_t TIA[64];
@@ -33,7 +37,51 @@ uint8_t readMemory(Mem2600 &memory, uint16_t adress)
     }
     else
     {
-        if(y == )
+        if(z <= 0x0 && z < 0x8)
+        {
+            return memory.TIA[adress & 0x003F];
+        }
+        else if(
+            y == 0x2 || y == 0x3 || y == 0x6 || y == 0x7 || y == 0xA || y == 0xB || y == 0xE || y == 0xF
+        )
+        {
+            return memory.RIOT[adress & 0x001F];
+        }
+        else
+        {
+            return memory.RAM[adress & 0x007F];
+        }
+    }
+}
+
+bool writeMemory(Mem2600 &memory, uint16_t adress, uint8_t value)
+{
+    int x = adress & 0xF000 >> 12;
+    int y = adress & 0x0F00 >> 8;
+    int z = adress & 0x00F0 >> 4;
+    if(x%2 == 1)
+    {
+        return false;
+    }
+    else
+    {
+        if(z <= 0x0 && z < 0x8)
+        {
+            memory.TIA[adress & 0x003F] = value;
+            return true;
+        }
+        else if(
+            y == 0x2 || y == 0x3 || y == 0x6 || y == 0x7 || y == 0xA || y == 0xB || y == 0xE || y == 0xF
+        )
+        {
+            memory.RIOT[adress & 0x001F] = value;
+            return true;
+        }
+        else
+        {
+            memory.RAM[adress & 0x007F] = value;
+            return true;
+        }
     }
 }
 
@@ -172,8 +220,6 @@ public:
     }
     uint16_t get_adress(std::vector<uint8_t> bytes, Mem2600 &memory)
     {
-        // n'utiliser cette methode que pour les instructions ou il y a un saut
-        // exemple : JMP, BCC, BCS, BEQ, BMI, BNE, BPL, BVC, BVS
         switch (this->adressMode)
         {
         case ZeroPage:
@@ -195,13 +241,13 @@ public:
             return (bytes[2] << 8) + bytes[1] + memory.Y;
             break;
         case Indirect:
-            return (memory.RAM[(bytes[2] << 8) + bytes[1] + 1] << 8) + memory.RAM[(bytes[2] << 8) + bytes[1]];
+            return (readMemory(memory, (bytes[2] << 8) + bytes[1] + 1) << 8) + readMemory(memory, (bytes[2] << 8) + bytes[1]);
             break;
         case IndirectX:
-            return (memory.RAM[(bytes[1] + memory.X + 1) % 0x100] << 8) + memory.RAM[(bytes[1] + memory.X)];
+            return (readMemory(memory, (bytes[1] + memory.X + 1) % 0x100) << 8) + readMemory(memory, (bytes[1] + memory.X) % 0x100);
             break;
         case IndirectY:
-            return (memory.RAM[(bytes[1] + 1) % 0x100] << 8) + memory.RAM[bytes[1]] + memory.Y;
+            return (readMemory(memory, bytes[1] + 1) << 8) + readMemory(memory, bytes[1]) + memory.Y;
             break;
         case Relative:
             return memory.PC + (int8_t)bytes[1];
@@ -215,13 +261,576 @@ public:
         };
         return 0;
     }
+    uint8_t getValue(std::vector<uint8_t> bytes, Mem2600 &memory)
+    {
+        uint8_t value = 0;
+        if (this->adressMode == Immediate)
+        {
+            value = bytes[1];
+        }
+        else if (this->adressMode == Accumulator)
+        {
+            value = memory.A;
+        }
+        else
+        {
+            value = readMemory(memory, get_adress(bytes, memory));
+        }
+    }
     void execute(std::vector<uint8_t> bytes, Mem2600 &memory)
     {
-        switch (this->opcode)
-        {
-            case "ADC":
 
-                break;
+        // Fait à l'aide de http://6502.org/tutorials/6502opcodes.html
+
+        if (this->name == "ADC")
+        {
+            uint8_t value = getValue(bytes, memory);
+
+            if (memory.D)
+            {
+                uint16_t result = memory.A + value + memory.C;
+                memory.C = result > 0x99;
+                memory.V = (~(memory.A ^ value) & (memory.A ^ result) & 0x80) != 0;
+                memory.N = (result & 0x80) != 0;
+                memory.Z = (result & 0xFF) == 0;
+                if ((memory.A & 0xF) + (value & 0xF) + memory.C > 0x9)
+                {
+                    result += 0x6;
+                }
+                memory.A = result;
+            }
+            else
+            {
+                uint16_t result = memory.A + value + memory.C;
+                memory.C = result > 0xFF;
+                memory.V = (~(memory.A ^ value) & (memory.A ^ result) & 0x80) != 0;
+                memory.N = (result & 0x80) != 0;
+                memory.Z = (result & 0xFF) == 0;
+                memory.A = result;
+            }
+        }
+
+        else if(this->name == "AND")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.A &= value;
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+        else if(this->name == "ASL")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.C = (value & 0x80) != 0;
+            value <<= 1;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            if(this->adressMode == Accumulator)
+            {
+                memory.A = value;
+            }
+            else
+            {
+                writeMemory(memory, get_adress(bytes, memory), value);
+            }
+        }
+
+        else if(this->name == "BIT")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.V = (value & 0x40) != 0;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = (memory.A & value) == 0;
+        }
+
+
+        // Branch instruction
+        else if (this->name == "BCC")
+        {
+            if (memory.C == 0)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BCS")
+        {
+            if (memory.C == 1)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BEQ")
+        {
+            if (memory.Z == 1)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BMI")
+        {
+            if (memory.N == 1)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BNE")
+        {
+            if (memory.Z == 0)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BPL")
+        {
+            if (memory.N == 0)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BVC")
+        {
+            if (memory.V == 0)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+        else if (this->name == "BVS")
+        {
+            if (memory.V == 1)
+            {
+                memory.PC = get_adress(bytes, memory);
+            }
+        }
+
+        else if(this->name == "BRK")
+        {
+            memory.B = 1;
+        }
+
+        else if(this->name == "CMP")
+        {
+            uint8_t value = getValue(bytes, memory);
+            uint16_t result = memory.A - value;
+            memory.C = memory.A >= value;
+            memory.N = (result & 0x80) != 0;
+            memory.Z = (result & 0xFF) == 0;
+        }
+        
+        else if(this->name == "CPX")
+        {
+            uint8_t value = getValue(bytes, memory);
+            uint16_t result = memory.X - value;
+            memory.C = memory.X >= value;
+            memory.N = (result & 0x80) != 0;
+            memory.Z = (result & 0xFF) == 0;
+        }
+
+        else if(this->name == "CPY")
+        {
+            uint8_t value = getValue(bytes, memory);
+            uint16_t result = memory.Y - value;
+            memory.C = memory.Y >= value;
+            memory.N = (result & 0x80) != 0;
+            memory.Z = (result & 0xFF) == 0;
+        }
+
+        else if(this->name == "DEC")
+        {
+            uint8_t value = getValue(bytes, memory);
+            value--;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            writeMemory(memory, get_adress(bytes, memory), value);
+        }
+
+        else if(this->name == "EOR")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.A ^= value;
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+                // Flags Instructions
+        else if (this->name == "BRK")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "RTI")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "PHP")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "PLP")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "PHA")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "PLA")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "DEY")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "TAY")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "INY")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "INX")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLC")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "SEC")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLI")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "SEI")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLV")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLD")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "SED")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLC")
+        {
+            memory.C = 0;
+        }
+        else if (this->name == "SEC")
+        {
+            memory.C = 1;
+        }
+        else if (this->name == "CLI")
+        {
+            memory.I = 0;
+        }
+        else if (this->name == "SEI")
+        {
+            memory.I = 1;
+        }
+        else if (this->name == "CLV")
+        {
+            memory.V = 0;
+        }
+        else if (this->name == "CLD")
+        {
+            memory.D = 0;
+        }
+        else if (this->name == "SED")
+        {
+            memory.D = 1;
+        }
+
+
+        else if(this->name == "INC")
+        {
+            uint8_t value = getValue(bytes, memory);
+            value++;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            writeMemory(memory, get_adress(bytes, memory), value);
+        }
+
+        // Jump instruction
+        else if (this->name == "JMP")
+        {
+            memory.PC = get_adress(bytes, memory);
+        }
+
+        // Jump instruction
+        else if (this->name == "JSR")
+        {
+            memory.PC -= 1;
+            writeMemory(memory, 0x100 + memory.SP, (memory.PC & 0xFF00) >> 8);
+            memory.SP--;
+            writeMemory(memory, 0x100 + memory.SP, memory.PC & 0x00FF);
+            memory.SP--;
+            memory.PC = get_adress(bytes, memory);
+        }
+
+        else if (this->name == "LDA")
+        {
+            memory.A = getValue(bytes, memory);
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+        else if (this->name == "LDX")
+        {
+            memory.X = getValue(bytes, memory);
+            memory.N = (memory.X & 0x80) != 0;
+            memory.Z = memory.X == 0;
+        }
+
+        else if (this->name == "LDY")
+        {
+            memory.Y = getValue(bytes, memory);
+            memory.N = (memory.Y & 0x80) != 0;
+            memory.Z = memory.Y == 0;
+        }
+
+        else if (this->name == "LSR")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.C = (value & 0x01) != 0;
+            value >>= 1;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            if (this->adressMode == Accumulator)
+            {
+                memory.A = value;
+            }
+            else
+            {
+                writeMemory(memory, get_adress(bytes, memory), value);
+            }
+        }
+        
+        else if (this->name == "NOP")
+        {
+
+        }
+
+        else if (this->name == "ORA")
+        {
+            uint8_t value = getValue(bytes, memory);
+            memory.A |= value;
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+        else if (this->name == "TAX")
+        {
+            memory.X = memory.A;
+            memory.N = (memory.X & 0x80) != 0;
+            memory.Z = memory.X == 0;
+        }
+
+        else if (this->name == "TXA")
+        {
+            memory.A = memory.X;
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+        else if (this->name == "DEX")
+        {
+            memory.X--;
+            memory.N = (memory.X & 0x80) != 0;
+            memory.Z = memory.X == 0;
+        }
+
+        else if (this->name == "INX")
+        {
+            memory.X++;
+            memory.N = (memory.X & 0x80) != 0;
+            memory.Z = memory.X == 0;
+        }
+
+        else if (this->name == "TAY")
+        {
+            memory.Y = memory.A;
+            memory.N = (memory.Y & 0x80) != 0;
+            memory.Z = memory.Y == 0;
+        }
+
+        else if (this->name == "TYA")
+        {
+            memory.A = memory.Y;
+            memory.N = (memory.A & 0x80) != 0;
+            memory.Z = memory.A == 0;
+        }
+
+        else if (this->name == "DEY")
+        {
+            memory.Y--;
+            memory.N = (memory.Y & 0x80) != 0;
+            memory.Z = memory.Y == 0;
+        }
+
+        else if (this->name == "INY")
+        {
+            memory.Y++;
+            memory.N = (memory.Y & 0x80) != 0;
+            memory.Z = memory.Y == 0;
+        }
+
+        else if (this->name == "ROL")
+        {
+            uint8_t value = getValue(bytes, memory);
+            uint8_t oldC = memory.C;
+            memory.C = (value & 0x80) != 0;
+            value <<= 1;
+            value += oldC;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            if (this->adressMode == Accumulator)
+            {
+                memory.A = value;
+            }
+            else
+            {
+                writeMemory(memory, get_adress(bytes, memory), value);
+            }
+        }
+
+        else if (this->name == "ROR")
+        {
+            uint8_t value = getValue(bytes, memory);
+            uint8_t oldC = memory.C;
+            memory.C = (value & 0x01) != 0;
+            value >>= 1;
+            value += oldC << 7;
+            memory.N = (value & 0x80) != 0;
+            memory.Z = value == 0;
+            if (this->adressMode == Accumulator)
+            {
+                memory.A = value;
+            }
+            else
+            {
+                writeMemory(memory, get_adress(bytes, memory), value);
+            }
+        }
+
+        else if (this->name == "RTI")
+        {
+            memory.SP++;
+            memory.PC = readMemory(memory, 0x100 + memory.SP);
+            memory.SP++;
+            memory.PC += readMemory(memory, 0x100 + memory.SP) << 8;
+            memory.SP++;
+            memory.PC += 1;
+        }
+
+        else if (this->name == "RTS")
+        {
+            memory.SP++;
+            memory.PC = readMemory(memory, 0x100 + memory.SP);
+            memory.SP++;
+            memory.PC += readMemory(memory, 0x100 + memory.SP) << 8;
+        }
+
+        else if (this->name == "SBC")
+        {
+            uint8_t value = getValue(bytes, memory);
+            if (memory.D)
+            {
+                uint16_t result = memory.A - value - (1 - memory.C);
+                memory.C = result < 0x100;
+                memory.V = ((memory.A ^ result) & (memory.A ^ value) & 0x80) != 0;
+                memory.N = (result & 0x80) != 0;
+                memory.Z = (result & 0xFF) == 0;
+                if ((memory.A & 0xF) - (value & 0xF) - (1 - memory.C) < 0)
+                {
+                    result -= 0x6;
+                }
+                memory.A = result;
+            }
+            else
+            {
+                uint16_t result = memory.A - value - (1 - memory.C);
+                memory.C = result < 0x100;
+                memory.V = ((memory.A ^ result) & (memory.A ^ value) & 0x80) != 0;
+                memory.N = (result & 0x80) != 0;
+                memory.Z = (result & 0xFF) == 0;
+                memory.A = result;
+            }
+        }
+        
+        else if (this->name == "STA")
+        {
+            writeMemory(memory, get_adress(bytes, memory), memory.A);
+        }
+
+        // Stack Instructions
+        else if (this->name == "TSX")
+        {
+            memory.SP = memory.X;
+        }
+
+        else if (this->name == "TSY")
+        {
+            memory.SP = memory.Y;
+        }
+
+        else if (this->name == "PHA")
+        {
+            writeMemory(memory, 0x100 + memory.SP, memory.A);
+        }
+
+        else if (this->name == "PLA")
+        {
+            memory.A = readMemory(memory, 0x100 + memory.SP);
+        }
+
+        else if (this->name == "PHP")
+        {
+            uint8_t flags = 0;
+            flags |= memory.C << 0;
+            flags |= memory.Z << 1;
+            flags |= memory.I << 2;
+            flags |= memory.D << 3;
+            flags |= memory.B << 4;
+            flags |= 0x1 << 5;
+            flags |= memory.V << 6;
+            flags |= memory.N << 7;
+
+            writeMemory(memory, 0x100 + memory.SP, flags);
+            
+
+        }
+
+        else if (this->name == "PLP")
+        {
+            uint8_t flags = readMemory(memory, 0x100 + memory.SP);
+            memory.C = flags & 0x1;
+            memory.Z = flags & 0x2;
+            memory.I = flags & 0x4;
+            memory.D = flags & 0x8;
+            memory.B = flags & 0x10;
+            memory.V = flags & 0x40;
+            memory.N = flags & 0x80;
+        }
+
+        else if (this->name == "STX")
+        {
+            writeMemory(memory, get_adress(bytes, memory), memory.X);
+        }
+
+        else if (this->name == "STY")
+        {
+            writeMemory(memory, get_adress(bytes, memory), memory.Y);
+        }
+
     }
 private:
     std::string name;
